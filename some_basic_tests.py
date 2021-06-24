d080b0a4540428357740b1996814a8c3fd3b23a0
Institute of Coding (Sunderland) <gavin.mcclary@sunderland.ac.uk>
2021-06-24 10:45:53 +0100

    Update some_basic_tests.py

--- some_basic_tests.py
+++ some_basic_tests.py
@@ -6,11 +6,7 @@ from some_basic_code import my_basic_function
 # Unittest boilerplate
 class TestStringMethods(unittest.TestCase):
 
-    # We want one test to fail to demonstrate CI build failure
-    def test_my_basic_function_fails(self):
-        self.assertEqual(my_basic_function(), "") # AssertionError
-
-    # And one test to pass
+    # Only one test and it shold pass - meaning build will be successful!
     def test_my_basic_function_passes(self):
         self.assertEqual(my_basic_function(), "Hi from my basic function!") # Pass!
 
